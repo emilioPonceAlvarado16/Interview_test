478b6279513a8be956ac1702d280ecf423e4c318
Isaias Ponce <isaiasponce-12@hotmail.es>
2021-11-26 12:42:22 -0500

    get_results function : 100%

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from datetime import datetime
 
 def format_validator(time):
     
-    pattern=r"^(MO|TU|WE|TH|FR|SA|SU)([0-1][0-9]|[2][0-3]):([0-5][0-9])\s*-\s*([0-1][0-9]|[2][0-3]):([0-5][0-9])$"
+    # pattern=r"^(MO|TU|WE|TH|FR|SA|SU)([0-1][0-9]|[2][0-3]):([0-5][0-9])\s*-\s*([0-1][0-9]|[2][0-3]):([0-5][0-9])$"
+    pattern=r"^(MO|TU|WE|TH|FR|SA|SU).?([0-1][0-9]|[2][0-3]):([0-5][0-9])\s*-\s*.?([0-1][0-9]|[2][0-3]):([0-5][0-9])$"
     groups=re.search(pattern, time)
+    
     if(not groups):return False #if Empty search, then return False
 
     groups=groups.group(1,2,3,4,5)
@@ -28,47 +30,48 @@ def is_time_match(start1, end1,start2,end2):
     else:
         return True
     
-
-file=open("hola.txt","r")
-
-time="MO10:00-12:00"
+def file_to_records(filename="records.txt"):
+    file=open(filename,"r")
 
 
-lines=file.readlines()
-file.close()
-records_list=[]
-pre_register=dict()
-for line in lines:
-    if(line!="\n"):
-        line=line.strip("\n")
-        line=line.split("=")
-        name=line[0]
-        record=line[1]
-        pre_register[name]= dict()
+
+    records_list=[]
+    lines=file.readlines()
+    file.close()
+    records_list=[]
+    pre_register=dict()
+    for line in lines:
+        if(line!="\n"):
+            line=line.strip("\n")
+            line=line.split("=")
+            name=line[0]
+            record=line[1]
+            pre_register[name]= dict()
         
-        for day_record in record.split(","):
+            for day_record in record.split(","):
            
             
-            day,start1,end=format_validator(day_record)            
-            pre_register[name][day]={"start":start1,"end":end}
+                day,start1,end=format_validator(day_record)            
+                pre_register[name][day]={"start":start1,"end":end}
             
         
-        records_list.append({name:pre_register[name]})
-        
-dic=[{'RENE': {'MO': {'start': '10:00', 'end': '12:00'}, 'TU': {'start': '10:00', 'end': '12:00'}, 'TH': {'start': '01:00', 'end': '03:00'}, 'SA': {'start': '14:00', 'end': '18:00'}, 'SU': {'start': '20:00', 'end': '21:00'}}}, {'ASTRID': {'MO': {'start': '10:00', 'end': '12:00'}, 'TH': {'start': '12:00', 'end': '14:00'}, 'SU': {'start': '20:00', 'end': '21:00'}}}, {'ANDRES': {'MO': {'start': '10:00', 'end': '12:00'}, 'TH': {'start': '12:00', 'end': '14:00'}, 'SU': {'start': '20:00', 'end': '21:00'}}}]
+            records_list.append({name:pre_register[name]})
+    return records_list
 
 
 
 def find_matches(person1, person2):
+    #{'ASTRID-RENE': 2}
     dicctionary=dict()
     name1=list(person1.keys())[0]
     name2=list(person2.keys())[0]
+    person1=person1[name1]
+    person2=person2[name2]
 
-
-    for key, value in person1[name1].items():
-        if(key in person2[name2].keys()):
-            records_person1=person1[name1][key]
-            records_person2=person2[name2][key]
+    for key, value in person1.items():
+        if(key in person2.keys()):
+            records_person1=person1[key]
+            records_person2=person2[key]
             match=is_time_match(records_person1["start"], records_person1["end"],records_person2["start"], records_person2["end"])
             if(match):
                 
@@ -79,17 +82,18 @@ def find_matches(person1, person2):
                     dicctionary[pair_names]=dicctionary[pair_names]+1
     return dicctionary
 
+def get_results(records_list):
+    n_records=len(records_list)
+    result=[]
+    for idx in range(n_records):
+        for idy in range(idx, n_records-1):
+            di=find_matches( records_list[idx], records_list[idy+1])
+            result.append(di)
 
+    return result
 
-    
+records=file_to_records()
+results=get_results(records)
 
-si=find_matches(dic[0], dic[1])
-print(si)
-# print(records_list)
-
-
-
-# print(is_time_match(start1, end1, start2, end2))
-
-
+print(results)
 
