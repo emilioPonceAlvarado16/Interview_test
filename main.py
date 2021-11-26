3834e6e604f2dadf54a2afb6c0f1140de9996fc9
Isaias Ponce <isaiasponce-12@hotmail.es>
2021-11-26 01:36:37 -0500

    find_matches function: done

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def format_validator(time):
     
     pattern=r"^(MO|TU|WE|TH|FR|SA|SU)([0-1][0-9]|[2][0-3]):([0-5][0-9])\s*-\s*([0-1][0-9]|[2][0-3]):([0-5][0-9])$"
     groups=re.search(pattern, time)
-    if(not groups):return False #Empty search, then return False
+    if(not groups):return False #if Empty search, then return False
 
     groups=groups.group(1,2,3,4,5)
     day=groups[0]
@@ -14,56 +14,10 @@ def format_validator(time):
     start_time=datetime.strptime(start, '%H:%M')
     end_time=datetime.strptime(end, '%H:%M')
 
-    if(start_time>end_time):return False #Start time is greater than end time, then return False
+    if(start_time>end_time):return False #if start time is greater than end time, then return False
 
    
-    return day,start,end
-    
-file=open("hola.txt","r")
-
-time="MO10:00-12:00"
-
-
-lines=file.readlines()
-file.close()
-
-records_register=dict()
-for line in lines:
-    if(line!="\n"):
-        #print(line)
-        line=line.strip("\n")
-        line=line.split("=")
-        name=line[0]
-        record=line[1]
-        records_register[name]= dict()
-        for day_record in record.split(","):
-            #print(day_record)
-            day,start1,end=format_validator(day_record)
-            #{day:{"start":start1,"end":end}}
-            
-            records_register[name][day]={"start":start1,"end":end}
-#print(records_register)
-
-dic={'RENE': {'MO': {'start': '10:00', 'end': '12:00'}}, 'ASTRID': {'MO': {'start': '10:00', 'end': '12:00'}}, 'ANDRES': {'MO': {'start': '10:00', 'end': '12:00'}}}
-def find_matches(dictionary):
-    match=dict()
-    for key,value in dictionary.items():
-        print(key,value)
-        pass
-
-    
-
-find_matches(records_register)
-start2="10:04"
-end2="23:02"
-
-start1="10:05"
-end1="23:30"
-
-start1=datetime.strptime(start1, '%H:%M')
-end1=datetime.strptime(end, '%H:%M')
-start2=datetime.strptime(start2, '%H:%M')
-end2=datetime.strptime(end2, '%H:%M')
+    return day,start_time,end_time
 
 def is_time_match(start1, end1,start2,end2):
     
@@ -75,6 +29,67 @@ def is_time_match(start1, end1,start2,end2):
         return True
     
 
-print(is_time_match(start1, end1, start2, end2))
+file=open("hola.txt","r")
+
+time="MO10:00-12:00"
+
+
+lines=file.readlines()
+file.close()
+records_list=[]
+pre_register=dict()
+for line in lines:
+    if(line!="\n"):
+        line=line.strip("\n")
+        line=line.split("=")
+        name=line[0]
+        record=line[1]
+        pre_register[name]= dict()
+        
+        for day_record in record.split(","):
+           
+            
+            day,start1,end=format_validator(day_record)            
+            pre_register[name][day]={"start":start1,"end":end}
+            
+        
+        records_list.append({name:pre_register[name]})
+        
+dic=[{'RENE': {'MO': {'start': '10:00', 'end': '12:00'}, 'TU': {'start': '10:00', 'end': '12:00'}, 'TH': {'start': '01:00', 'end': '03:00'}, 'SA': {'start': '14:00', 'end': '18:00'}, 'SU': {'start': '20:00', 'end': '21:00'}}}, {'ASTRID': {'MO': {'start': '10:00', 'end': '12:00'}, 'TH': {'start': '12:00', 'end': '14:00'}, 'SU': {'start': '20:00', 'end': '21:00'}}}, {'ANDRES': {'MO': {'start': '10:00', 'end': '12:00'}, 'TH': {'start': '12:00', 'end': '14:00'}, 'SU': {'start': '20:00', 'end': '21:00'}}}]
+
+
+
+def find_matches(person1, person2):
+    dicctionary=dict()
+    name1=list(person1.keys())[0]
+    name2=list(person2.keys())[0]
+
+
+    for key, value in person1[name1].items():
+        if(key in person2[name2].keys()):
+            records_person1=person1[name1][key]
+            records_person2=person2[name2][key]
+            match=is_time_match(records_person1["start"], records_person1["end"],records_person2["start"], records_person2["end"])
+            if(match):
+                
+                pair_names=name2+"-"+name1
+                if(pair_names not in dicctionary):
+                    dicctionary[pair_names]=1
+                else:
+                    dicctionary[pair_names]=dicctionary[pair_names]+1
+    return dicctionary
+
+
+
+    
+
+si=find_matches(dic[0], dic[1])
+print(si)
+# print(records_list)
+
+
+
+# print(is_time_match(start1, end1, start2, end2))
+
 
 
